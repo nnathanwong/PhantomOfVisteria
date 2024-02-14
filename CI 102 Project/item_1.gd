fe9e28810706988fee92aca7c8c1f703a360bf1b
Nathan Wong <njw87@drexel.edu>
2024-02-14 18:48:05 -0500

    Afrah told me to do this

---
+++ CI 102 Project/item_1.gd
@@ -0,0 +1,12 @@
+extends Button
+
+var list_item_1 = "item_1"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	text = list_item_1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ CI 102 Project/skill_1.gd
@@ -0,0 +1,12 @@
+extends Button
+
+var skill_1 = "Fire"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	text = skill_1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
