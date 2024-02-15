550e0c294e6658403edf0d0a48f60cb6525f5194
Dewei <ds3862@drexel.edu>
2024-02-14 22:31:47 -0500

    Merged Enemies into Tile_map

---
+++ temp/New Game Project-Aaron/GitGodot1/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var speed = 10
+var player = null
+var detection = false
+
+func _physics_process(_delta):
+	if detection:
+		position += (player.position - position).normalized() * speed
+		$Slime.play("walk")
+	else:
+		$Slime.play("idle")
+
+func _on_detection_body_entered(body):
+	player = body
+	detection = true
+
+
+func _on_detection_body_exited(body):
+	player = null
+	detection = false
