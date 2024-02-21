798ff615bdaf3b1ffdc7d9e6cdf85479180440f2
an953 <an953@drexel.edu>
2024-02-21 09:53:00 -0500

    Switches to battle scene on player's collision with enemy

--- Main Project/GitGodot1/enemy.gd
+++ Main Project/GitGodot1/enemy.gd
@@ -10,12 +10,13 @@ func _physics_process(_delta):
 		$Slime.play("walk")
 	else:
 		$Slime.play("idle")
-
+	
 func _on_detection_body_entered(body):
 	player = body
 	detection = true
 
-
 func _on_detection_body_exited(body):
 	player = null
 	detection = false
+	
+	get_tree().change_scene_to_file("res://GitGodot1/BattleScene.tscn") # Switch to battle scene
