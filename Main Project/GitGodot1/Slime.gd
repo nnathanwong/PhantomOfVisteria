9dd5527b23386f624088d9e4857c42a74edd521b
Dewei <ds3862@drexel.edu>
2024-03-03 17:29:10 -0500

    added hurt animations to the turn based comabat

--- Main Project/GitGodot1/Slime.gd
+++ Main Project/GitGodot1/Slime.gd
@@ -1,6 +1,11 @@
 extends AnimatedSprite2D
 
 var HP
+@onready var hurt = $hurteffect
+@onready var damaged = $damage_timer
+
+func _ready():
+	play("idle")
 
 func _init():
 	HP = 100
@@ -16,4 +21,9 @@ func _on_selection_pressed():
 	var damage_taken = 5 * randi_range(2,10)
 	print(damage_taken)
 	HP -= damage_taken
+	hurt.play("hurt_animation")
+	damaged.start()
+	await damaged.timeout
+	hurt.play("RESET")
+	
 	signals.nextTurn.emit()
