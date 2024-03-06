098d87a52da48e03b8dfe9efbe7088d8b68bebf5
Dewei <ds3862@drexel.edu>
2024-03-05 20:44:50 -0500

    finished enemy movement + scene transition

--- Main Project/GitGodot1/enemy.gd
+++ Main Project/GitGodot1/enemy.gd
@@ -1,31 +1,32 @@
 extends CharacterBody2D
 
-var speed = 3
-var player_chase = false
-var player = null
-var playerpos = null
-
-func _ready():
-	playerpos = get_parent().get_node("player").get_position()
+var speed = 90
+var detection = false
+@onready var playerpos = get_node("../player")
 
 func _physics_process(delta):
-	var direction = Vector2.ZERO
-	if player_chase and player:
-		direction = (playerpos - direction).normalized()
-		position += direction * speed * delta
+	#velocity = Vector2.ZERO
+	if detection:
+		var direction = (playerpos.global_position - self.global_position).normalized()
+		velocity = direction * speed
 		$Slime.play("walk")
-	
-		if direction.x < 0:
+		if (playerpos.position.x - position.x) < 0:
 			$Slime.flip_h = true
 		else:
 			$Slime.flip_h = false
 	else:
+		velocity = Vector2.ZERO
 		$Slime.play("idle")
+	move_and_slide()
 
 func _on_detection_body_entered(body):
-	player = body
-	player_chase = true
+	if body.name == "player":
+		detection = true
 
 func _on_detection_body_exited(body):
-	player = null
-	player_chase = null
+	if body.name == "player":
+		detection = false
+
+func _on_collision_body_entered(body):
+	if body.name == "player":
+		get_tree().change_scene_to_file("res://GitGodot1/BattleScene.tscn")
