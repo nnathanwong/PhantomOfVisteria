d42db3857a2e7c5a918b3af17ea95e2c1a5fc7b0
Nathan Wong <njw87@drexel.edu>
2024-05-01 10:28:18 -0400

    Reworked partyStats.gd by removing class_name and class [character]

--- Main Project/globals/attackStats.gd
+++ Main Project/globals/attackStats.gd
@@ -2,29 +2,27 @@ extends Resource
 
 class_name attackStats
 
-class Attacks:
-	var multiplier = preload("res://globals/partyStats.gd")
-	var normal_damage : int = 5
-	var magic_damage : int = 5 
+var multiplier = preload("res://globals/partyStats.gd")
+var normal_damage : int = 5
+var magic_damage : int = 5 
 
-	func strength(name):
-		# Modified by Nathan 4/24/2024
-		if name == 0:
-			var ancel = multiplier.Ancel.strength + normal_damage
-			return ancel
-		elif name == 1:
-			var lorane = multiplier.Lorane.strength + normal_damage
-			return lorane
-		elif name == 2:
-			var raflahn = multiplier.Raflahn.strength + normal_damage
-			return raflahn
-		elif name == 3:
-			var younn = multiplier.Younn.strength + normal_damage
-			return younn
-		
+func strength(name):
+	# Modified by Nathan 4/24/2024
+	if name == 0:
+		var ancel = PartyMemberStats.Ancel.strength + normal_damage
+		return ancel
+	elif name == 1:
+		var lorane = multiplier.Lorane.strength + normal_damage
+		return lorane
+	elif name == 2:
+		var raflahn = multiplier.Raflahn.strength + normal_damage
+		return raflahn
+	elif name == 3:
+		var younn = multiplier.Younn.strength + normal_damage
+		return younn
 
-	func magic():
-		var ancel = multiplier.Ancel.magic + magic_damage
-		var lorane = multiplier.Lorane.magic + magic_damage
-		var raflahn = multiplier.Raflahn.magic + magic_damage
-		var younn = multiplier.Younn.magic + magic_damage
+func magic():
+	var ancel = multiplier.Ancel.magic + magic_damage
+	var lorane = multiplier.Lorane.magic + magic_damage
+	var raflahn = multiplier.Raflahn.magic + magic_damage
+	var younn = multiplier.Younn.magic + magic_damage
--- Main Project/globals/partyStats.gd
+++ Main Project/globals/partyStats.gd
@@ -1,5 +1,70 @@
 extends Resource
 
+# STATS FOR ANCEL
+# Max stats
+var ancel_max_hp = 100
+var ancel_max_sp = 10
+# Current stats
+var ancel_hp = ancel_max_hp
+var ancel_sp = ancel_max_sp
+var ancel_defense = 10
+var ancel_strength = 16
+var ancel_magic = 3
+var ancel_poison = false
+var ancel_burn = false
+var ancel_bleed = false
+var ancel_fear = false
+var ancel_confusion = false
+
+# STATS FOR LORANE
+# Max stats
+var lorane_max_hp =  126
+var lorane_max_sp = 40
+# Current stats
+var lorane_hp = lorane_max_hp
+var lorane_sp = lorane_max_sp
+var lorane_defense = 14
+var lorane_strength = 6
+var lorane_magic = 21
+var lorane_poison = false
+var lorane_burn = false
+var lorane_bleed = false
+var lorane_fear = false
+var lorane_confusion = false
+
+# STATS FOR RAFLAHN
+# Max stats
+var raflahn_max_hp : int = 180
+var raflahn_max_sp : int = 50
+# Current stats
+var raflahn_hp = raflahn_max_hp
+var raflahn_sp = raflahn_max_sp
+var raflahn_defense = 15
+var raflahn_strength = 10
+var raflahn_magic = 24
+var raflahn_poison = false
+var raflahn_burn = false
+var raflahn_bleed = false
+var raflahn_fear = false
+var raflahn_confusion = false
+
+# STATS FOR YOUNN
+# Max stats
+var younn_max_hp = 230
+var younn_max_sp = 20
+# Current stats
+var younn_hp = younn_max_hp
+var younn_sp = younn_max_sp
+var younn_defense = 12
+var younn_strength = 16
+var younn_magic = 18
+var younn_poison = false
+var younn_burn = false
+var younn_bleed = false
+var younn_fear = false
+var younn_confusion = false
+
+'''
 class_name PartyStats
 
 class Ancel:
@@ -453,3 +518,4 @@ class Younn:
 	func levelMagic(magic, new):
 		magic = new
 		return magic
+'''
