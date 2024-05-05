5141571c148ba2135f1c65e3015ec108faf35419
Dewei <ds3862@drexel.edu>
2024-05-05 16:49:20 -0400

    added new folder with fonts to be used in the game, also created new scene and script to handle main menu objects

---
+++ Main Project/Menu.gd
@@ -0,0 +1,15 @@
+extends Control
+'''This script is to change the scenes depending on what button has been clicked'''
+#Dewei 5/5/2023
+
+#Button Font used: https://fonts.google.com/specimen/Pixelify+Sans?query=pixel;
+func _on_start_pressed(): #start game, change scene to tile_map
+	get_tree().change_scene_to_file("res://GitGodot1/tile_map_REMOTE_435.tscn")
+
+
+func _on_credits_pressed(): #show credits
+	pass # Replace with function body.
+
+
+func _on_quit_pressed(): #quit game window
+	get_tree().quit()
